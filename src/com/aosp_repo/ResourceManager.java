bc2a2965077557cecf7c9aa4faf3d2702c01726d
BeChris100 <bechris.100@outlook.com>
2022-09-16 16:14:59 +0200

    ResourceManager.java: Checking for resources

--- src/com/aosp_repo/ResourceManager.java
+++ src/com/aosp_repo/ResourceManager.java
@@ -1,9 +1,8 @@
 package com.aosp_repo;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import com.aosp_repo.utils.FileUtil;
+
+import java.io.*;
 import java.net.URL;
 import java.nio.file.AccessDeniedException;
 import java.util.Objects;
@@ -15,20 +14,35 @@ public class ResourceManager {
     }
 
     public static String getPath(String resPath) {
+        if (ResourceManager.class.getResource(resPath) == null)
+            throw new RuntimeException("Resource \"" + resPath + "\" does not exist");
+
         return Objects.requireNonNull(ResourceManager.class.getResource(resPath)).getPath();
     }
 
     public static URL getResourceUrl(String resPath) {
+        if (ResourceManager.class.getResource(resPath) == null)
+            throw new RuntimeException("Resource \"" + resPath + "\" does not exist");
+
         return ResourceManager.class.getResource(resPath);
     }
 
     public static InputStream openStream(String resPath) {
+        if (ResourceManager.class.getResource(resPath) == null)
+            throw new RuntimeException("Resource \"" + resPath + "\" does not exist");
+
         return ResourceManager.class.getResourceAsStream(resPath);
     }
 
     public static void copyOut(String resPath, File outFile) throws IOException {
+        if (ResourceManager.class.getResource(resPath) == null)
+            throw new RuntimeException("Resource \"" + resPath + "\" does not exist");
+
+        if (!outFile.exists())
+            FileUtil.createFile(outFile.getPath(), true);
+
         if (outFile.exists() && !outFile.canWrite())
-            throw new AccessDeniedException("Could not rewrite \"" + outFile.getPath() + "\"");
+            throw new AccessDeniedException("Could not overwrite \"" + outFile.getPath() + "\"");
 
         InputStream input = openStream(resPath);
         FileOutputStream output = new FileOutputStream(outFile, false);
